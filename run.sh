5e6d17c29afc0c18ddabb6a7cae6f4ad8bf32975
ceilingrat <>
2020-12-14 04:26:22 +0000

    create commit from commandline, push from graphical UI

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 
-echo -e "Committing to GitHub\n\nSupply a Comment:"
+echo -e "Commit to GitHub\n\nSupply a Comment:"
 read -e comment
 
 git add .
 git commit --author 'ceilingrat <>' -m "$comment"
 # git push -u origin main
+
+echo -e "ATTENTION\nCommit created, but NOT YET pushed to GitHub.\n\nGo to 'Version Control' panel to push commit."
