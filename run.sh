6a9b21837a344bf04fdb951c97380919008edb1f
ceilingrat <mail@ceilingrat.donotreadthecomments.com>
2020-12-14 04:19:21 +0000

    test version control UI of repl.it

--- run.sh
+++ run.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -x
-# commit to github using Repl.it
-
-# git config --global user.name "ceilingrat"
-# git config --global user.email '<>'
-
-# cat .git/config
 
 echo -e "Committing to GitHub\n\nSupply a Comment:"
 read -e comment
