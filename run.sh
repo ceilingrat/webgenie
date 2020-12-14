a4fd7b85ae33298e254cbfb7f4ad2e4719dbc3eb
ceilingrat <>
2020-12-14 05:51:20 +0000

    moved username to variable

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -x
 
+username="ceilingrat"
+
 echo -e "Commit to GitHub\n\nSupply a Comment:"
 read -e comment
 
 git add .
-git commit --author 'ceilingrat <>' -m "$comment"
+git commit --author "$username <>" -m "$comment"
 # git push -u origin main
 
 echo -e "ATTENTION\nCommit created, but NOT YET pushed to GitHub.\n\nGo to 'Version Control' panel to push commit."
