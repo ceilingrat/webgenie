ba0902920b9e6041fa817513b7205fc8a2051ddc
ceilingrat <>
2020-12-14 05:39:09 +0000

    new source complete

--- webgenie.js
+++ webgenie.js
@@ -36,7 +36,6 @@
     srcPrefix : "https://output.jsbin.com/",
     srcSuffix : ".js"
   },
-  /* functions with no side-effects */
   fn : {
     ReduceObjContructorArrayItem ( pAcc, pVal ) {
         if ( "fn" in pVal && 
@@ -48,29 +47,36 @@
             pAcc[fnX.name] = fnX;                
         }
         return pAcc;
-    },
-    SrcToScriptElm : function ( pStrSrc ) {
-      var oScript = document.createElement("script");
-      oScript.setAttribute("src", pStrSrc);
-      return oScript;           
     }
   },
-  /* object constructors */
   o : [
-      /*
-          {
-              fn : function Foo ( pStr1 ) {
-
-              },
-              prototype : {
-                  bar : function () {},
-                  baz : function () {}
-              }
-          }
-      */
+    {
+      fn : function Src ( oData ) {
+        var private = {};
+        for ( var x in oData ) private[x] = oData[x];   
+        if ( !private.srcFull ) {
+          if ( !private.code ) private.code = prompt( "Paste script code" );
+          private.srcFull = private.srcPrefix + private.code + private.srcSuffix;
+        }
+        this.getSrcFull = function () { return private.srcFull; }
+        this.getConfirm = function () { return private.confirm; }
+      },
+      prototype : {
+        insertScript : function () {
+          var bConfirm = this.getConfirm(), sSrcFull, oScript;
+          if ( bConfirm ) sSrcFull = prompt( "Confirm script URL\nIf necessary, edit URL text below", this.getSrcFull() );
+          if ( !sSrcFull ) return;
+          oScript = document.createElement("script");
+          oScript.setAttribute("src", sSrcFull);           
+          document.body.append( oScript );
+          console.log( "script inserted: ", sSrcFull );
+        }
+      }
+    }
   ],
-  /* main execution, incl. local variables */
   run : function () {
       var data = this.data, fn = this.fn, o = this.o.reduce( fn.ReduceObjContructorArrayItem , {} );
+      var oSrc1 = new o.Src(data);
+      oSrc1.insertScript();
   }
 }).run();
