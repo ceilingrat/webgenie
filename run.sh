08508ed1c9d330e80c59b122941309c3eaa196d9
ceilingrat <>
2020-12-14 08:36:46 +0000

    formatting of command-line message

--- run.sh
+++ run.sh
@@ -9,4 +9,4 @@ git add .
 git commit --author "$username <>" -m "$comment"
 # git push -u origin main
 
-echo -e "ATTENTION\nCommit created, but NOT YET pushed to GitHub.\n\nGo to 'Version Control' panel to push commit."
+echo -e "\n\n!~ATTENTION~!\nCommit created, but NOT YET pushed to GitHub.\n\nGo to 'Version Control' panel to push commit."
